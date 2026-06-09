661532f4f1ab84b731c2a6f87b520efdf459afa2
roms831 <pd28984@gmail.com>
2026-06-08 20:45:40 -0500

    Add files via upload
    Actualizacion de la configuración estática en WebConfig.java para permitir que las imágenes de la carpeta Descargas puedan ser leídas por el navegador de forma pública.

--- backend/src/main/java/com/tambo/sistematambo/config/WebConfig.java
+++ backend/src/main/java/com/tambo/sistematambo/config/WebConfig.java
@@ -9,9 +9,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
+                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "http://192.168.*.*:*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
     }
 }
